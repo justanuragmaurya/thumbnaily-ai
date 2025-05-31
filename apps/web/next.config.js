916c1ebd8392b4268926f3298910febe7324884e
justanuragmaurya <justanuragmaurya@gmail.com>
2025-05-31 11:42:14 +0000

    config update

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,20 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'thumbnaily.s3.ap-south-1.amazonaws.com',
-          pathname: '**',
-        },
-      ],
-    },
-  };
+  allowedDevOrigins: [
+    'http://165.22.214.246',
+    'http://thumbnaily.anuragmaurya.com',
+    'https://thumbnaily.anuragmaurya.com'
+  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'thumbnaily.s3.ap-south-1.amazonaws.com',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  // Other Next.js config options...
+
+  allowedDevOrigins: [
+    'http://165.22.214.246',
+    'http://thumbnaily.anuragmaurya.com'
+    'https://thumbnaily.anuragmaurya.com'
+  ],
+};
+
+module.exports = nextConfig;
+
