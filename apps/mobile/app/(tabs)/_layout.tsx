ab166b937112860348a1a015deea25ed8ac5c069
Anurag Maurya <justanuragmaurya@gmail.com>
2025-09-21 12:04:45 +0530

    added ai generations

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -1,77 +1,124 @@
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { Image, ImageBackground, Text, View } from 'react-native'
+import React from "react";
+import { Tabs } from "expo-router";
+import { Image, ImageBackground, Text, View } from "react-native";
 
 const _layout = () => {
   return (
-    <Tabs screenOptions={{
-        tabBarShowLabel:false,
-        tabBarItemStyle:{
-            height:"100%",
-            width:"100%",
-            justifyContent:'center',
-            alignItems:'center'
-        }
-    }}>
-        <Tabs.Screen
-            name='index' 
-            options={{
-                title:"Home",
-                headerShown:false,
-                tabBarIcon:({focused})=>{
-                    return(
-                        <View style={{flex:1, alignItems:'center' , justifyContent:'center' ,height:"100%"}}>
-                            <Image source={require('../../assets/icons/home.png')} tintColor={focused?"#00acdf":"grey"} />
-                        </View>
-                    )
-                }
-            }}
-        />
-        <Tabs.Screen
-            name='generate' 
-            options={{
-                title:"Generate",
-                headerShown:false,
-                tabBarIcon:({focused})=>{
-                    return(
-                        <View style={{flex:1, alignItems:'center' , justifyContent:'center' ,height:"100%"}}>
-                            <Image source={require('../../assets/icons/generate.png')} tintColor={focused?"#00acdf":"grey"} />
-                        </View>
-                    )
-                }
-            }}
-            
-        />
-        <Tabs.Screen
-            name='public' 
-            options={{
-                title:"All Thumbnails",
-                headerShown:false,
-                tabBarIcon:({focused})=>{
-                    return(
-                        <View style={{flex:1, alignItems:'center' , justifyContent:'center' ,height:"100%"}}>
-                            <Image source={require('../../assets/icons/public.png')} tintColor={focused?"#00acdf":"grey"} />
-                        </View>
-                    )
-                }
-            }}
-        />
-        <Tabs.Screen
-            name='account' 
-            options={{
-                title:"Profile",
-                headerShown:false,
-                tabBarIcon:({focused})=>{
-                    return(
-                        <View style={{flex:1, alignItems:'center' , justifyContent:'center' ,height:"100%"}}>
-                            <Image source={require('../../assets/icons/account.png')} tintColor={focused?"#00acdf":"grey"} />
-                        </View>
-                    )
-                }
-            }}
-        />
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#0a0a0a",
+          borderTopColor: "#262626",
+          borderTopWidth: 2,
+          height: 60,
+        },
+        tabBarItemStyle: {
+          height: "100%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                }}
+              >
+                <Image
+                  source={require("../../assets/icons/home.png")}
+                  tintColor={focused ? "#00acdf" : "grey"}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="generate"
+        options={{
+          title: "Generate",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                }}
+              >
+                <Image
+                  source={require("../../assets/icons/generate.png")}
+                  tintColor={focused ? "#00acdf" : "grey"}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="public"
+        options={{
+          title: "All Thumbnails",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                }}
+              >
+                <Image
+                  source={require("../../assets/icons/public.png")}
+                  tintColor={focused ? "#00acdf" : "grey"}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                }}
+              >
+                <Image
+                  source={require("../../assets/icons/account.png")}
+                  tintColor={focused ? "#00acdf" : "grey"}
+                />
+              </View>
+            );
+          },
+        }}
+      />
     </Tabs>
-  )
-}
+  );
+};
 
-export default _layout
+export default _layout;
--- apps/mobile/app/(tabs)/generate.tsx
+++ apps/mobile/app/(tabs)/generate.tsx
@@ -1,14 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Image, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { fal } from "@fal-ai/client"
+const GeneratePage = () => {
+  const [prompt , setPrompt] = React.useState<string>("")
+  const [image , setImage] = React.useState<string|null>("")
+  
+  const generateThumbnail = async()=>{
+    console.log("Started")
+    if(!prompt){
+      alert("Please Enter a prompt");
+      return;
+    }
 
-const generate = () => {
+    fal.config({
+      credentials: process.env.EXPO_PUBLIC_FAL_API_KEY
+    });
+
+    const response = await fal.subscribe("fal-ai/flux/dev", {
+      input: {
+        prompt: prompt,
+        output_format: "png",
+        num_inference_steps:50,
+        image_size:{"width":1920,"height":1080}
+      },
+    });
+
+    setImage(response.data.images[0].url)
+    
+    console.log("DONE")
+  }
   return (
-    <View style={{flex:1, alignItems:'center', paddingTop:100}}>
-      <Text>Generate Thumbnails</Text>
-    </View>
+    <SafeAreaView className='flex-1 items-center bg-neutral-950 text-white p-5'>
+      <View className='flex-col items-center text-center mt-10'>
+        <Text className='text-6xl'>
+          🔥
+        </Text>
+      <Text className='text-white text-3xl font-bold'>
+        Idea to thumbnail in seconds.
+      </Text>
+      <Text className='text-white/60'>
+        Thumbnaily is your superhuman thumbnail artist.
+      </Text>
+      </View>
+      <View className='border border-neutral-700 w-full rounded-2xl p-2 mt-5'>
+        <TextInput className='p-2 h-12 text-start text-white' placeholder='Enter Your prompt here' onChangeText={setPrompt}/>
+        <View className='flex-row justify-between items-center'>
+          <TouchableOpacity><Text className='text-white bg-neutral-900 border border-neutral-700  p-4 rounded-xl py-3'>Attach Reference</Text></TouchableOpacity>
+          <TouchableOpacity><Text className='bg-white p-4 rounded-xl py-3' onPress={generateThumbnail}>Generate</Text></TouchableOpacity>
+        </View>
+      </View>
+      <View className='flex-1 items-center w-full mt-16'>
+        {image&&<Image 
+          source={{uri:image}}
+          className="w-full aspect-[1.77] object-contain rounded-md"
+        />}
+        {image&&<TouchableOpacity className=''><Text>Download</Text></TouchableOpacity>}
+      </View>
+    </SafeAreaView>
   )
 }
 
-export default generate
-
-const styles = StyleSheet.create({})
+export default GeneratePage
